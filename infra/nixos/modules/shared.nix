a092bd4de956300336ac905ec0e1a021613b3636
Light <farhan29110511@gmail.com>
2026-05-16 19:27:22 +0530

    nix: fix secrets relative path

--- infra/nixos/modules/shared.nix
+++ infra/nixos/modules/shared.nix
@@ -64,15 +64,15 @@
   #secrets
   age.secrets = {
     postgres-env = {
-      file = ../../secrets/postgres.env.age;
+      file = ../secrets/postgres.env.age;
       path = "/run/secrets/postgres.env";
     };
     umami-env = {
-      file = ../../secrets/umami.env.age;
+      file = ../secrets/umami.env.age;
       path = "/run/secrets/umami.env";
     };
     kcet-env = {
-      file = ../../secrets/kcet.env.age;
+      file = ../secrets/kcet.env.age;
       path = "/run/secrets/kcet.env";
     };
   };
