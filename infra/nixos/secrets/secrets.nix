6c97e7f422b05571dc92b3ed899e8847819be747
Light <farhan29110511@gmail.com>
2026-05-16 19:41:24 +0530

    rekey secrets with vps host key

--- infra/nixos/secrets/secrets.nix
+++ infra/nixos/secrets/secrets.nix
@@ -1,9 +1,13 @@
 let
   me = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBT9k5Xs55jgP9TfndMA8hGhZyec0lQeJY+oDlBYfeuW agenix secrets key";
-
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsGHxVmg2CpCJLZpwX/Dnqbk/lAKWGM1Z2sf7A4gQIV root@nixchan";
+  all = [
+    me
+    server
+  ];
 in
 {
-  "postgres.env.age".publicKeys = [ me ];
-  "umami.env.age".publicKeys = [ me ];
-  "kcet.env.age".publicKeys = [ me ];
+  "postgres.env.age".publicKeys = all;
+  "umami.env.age".publicKeys = all;
+  "kcet.env.age".publicKeys = all;
 }
